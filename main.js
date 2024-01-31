bde1c1e605cbd6175fcff94a487b0ba1be111fb8
Madhur Pathak <madhurpathak000@gmail.com>
2024-01-31 14:39:13 +0530

    maxMinPrice

--- main.js
+++ main.js
@@ -99,4 +99,21 @@ const setCategories = () => {
   });
 };
 
+const setPrice = () => {
+  const priceList = data.map((item) => item.price);
+  const minPrice = Math.min(...priceList);
+  const maxPrice = Math.max(...priceList);
+
+  priceRange.min = minPrice;
+  priceRange.max = maxPrice;
+  priceRange.value = maxPrice;
+  priceValue.textContent = "₹" + maxPrice;
+
+  priceRange.addEventListener("input", (e) => {
+    priceValue.textContent = "₹" + e.target.value;
+    displayProducts(data.filter((item) => item.price <= e.target.value));
+  });
+};
+
 setCategories();
+setPrice();
